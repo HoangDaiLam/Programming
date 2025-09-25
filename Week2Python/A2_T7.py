58c354dac77f32421f6db05003732da35000f2d4
HoangDaiLam <hoangdailam1225@gmail.com>
2025-09-25 09:01:41 +0300

    update my files

---
+++ Week2Python/A2_T7.py
@@ -0,0 +1,6 @@
+print("Program starting.")
+temp = float(input("Insert fahrenheits: "))
+Celcius = (temp -32)/1.8
+Celcius = round(Celcius,1)
+print(f"{temp} ℉ is {Celcius} ℃")
+print("Program ending.")
